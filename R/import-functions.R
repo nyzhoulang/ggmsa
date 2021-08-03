ef50a839602505201f6114c152ac6e0a640fd4f4
nyzhoulang <nyzhoulang@163.com>
2021-08-03 12:10:13 +0800

    fixed check

--- R/import-functions.R
+++ R/import-functions.R
@@ -5,13 +5,28 @@ utils::globalVariables("read.delim") #arc.R
 utils::globalVariables(c("name", "position_adj", "y_adj")) #SeqBundles.R
 
 #arc.R
-readHelix <- getFromNamespace("readHelix", "R4RNA")
-readConnect <- getFromNamespace("readConnect", "R4RNA")
-readVienna <- getFromNamespace("readVienna", "R4RNA")
-readBpseq <- getFromNamespace("readBpseq", "R4RNA")
+##' @importFrom R4RNA readHelix
+##' @importFrom R4RNA readConnect
+##' @importFrom R4RNA readVienna
+##' @importFrom R4RNA readBpseq
+##' @importFrom R4RNA expandHelix
+##' @importFrom R4RNA collapseHelix
 
-expandHelix <- getFromNamespace("expandHelix", "R4RNA")
-collapseHelix <- getFromNamespace("collapseHelix", "R4RNA")
+R4RNA::readHelix
+R4RNA::readConnect
+R4RNA::readVienna
+R4RNA::readVienna
+R4RNA::expandHelix
+R4RNA::collapseHelix
+
+# readHelix <- getFromNamespace("readHelix", "R4RNA")
+# readConnect <- getFromNamespace("readConnect", "R4RNA")
+# readVienna <- getFromNamespace("readVienna", "R4RNA")
+# readBpseq <- getFromNamespace("readBpseq", "R4RNA")
+#
+# expandHelix <- getFromNamespace("expandHelix", "R4RNA")
+# collapseHelix <- getFromNamespace("collapseHelix", "R4RNA")
+
 # colourByValue <- getFromNamespace("colourByValue", "R4RNA")
 # colourByCount <- getFromNamespace("colourByCount", "R4RNA")
 # colourByBasepairFrequency <- getFromNamespace("colourByBasepairFrequency", "R4RNA")
